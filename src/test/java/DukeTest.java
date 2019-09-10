8fff2e313518223a35e2a1a855b365014620b94d
Krazzen <e0310417@u.nus.edu>
2019-09-10 11:52:10 +0800

    A-TextUiTesting

--- src/test/java/DukeTest.java
+++ src/test/java/DukeTest.java
@@ -6,10 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DukeTest {
     @Test
     public void Todo_Test(){
-        String in = "Todo 123";
-        String out = "Got it. I've added this task:\n" +
-                "[T][✘] 123\n" +
-                "Now you have 4 tasks in the lists.";
-        assertEquals(in, out);
+        assertEquals( "[T][\u2718] Test Todo", (new Todo("Test Todo").toString())
     }
 }
